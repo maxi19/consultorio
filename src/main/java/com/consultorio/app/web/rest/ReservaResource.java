f1948ad3f22ee29480c47f8ce25e99469da28f58
maxi <maxi19@gmail.com>
2021-09-10 19:57:01 -0300

    se agrega pdf conf inicial

--- src/main/java/com/consultorio/app/web/rest/ReservaResource.java
+++ src/main/java/com/consultorio/app/web/rest/ReservaResource.java
@@ -1,16 +1,26 @@
 package com.consultorio.app.web.rest;
 
+import com.consultorio.app.domain.Reserva;
 import com.consultorio.app.helpers.RangoHorario;
 import com.consultorio.app.manager.ReservaManager;
+import com.consultorio.app.pdf.PdfComponent;
 import com.consultorio.app.service.dto.ReservaDto;
+import com.netflix.client.http.HttpResponse;
+import lombok.experimental.var;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.ribbon.okhttp.OkHttpRibbonResponse;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.ByteArrayInputStream;
 import java.util.List;
 import java.util.Map;
 
@@ -54,5 +64,22 @@ public class ReservaResource {
     public ResponseEntity<Void> eliominarReserva(@Valid  @RequestBody ReservaDto reservaDto) throws Exception {
         return reservaManager.eliminarReserva(reservaDto);
     }
+
+
+    @RequestMapping(value = "/externos/reservas/comprobante/pdfreport", method = RequestMethod.GET, produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<Object> imprimir() {
+        Reserva reserva = new Reserva();
+        reserva.setId(new Long("1"));
+        reserva.setNombre("maxi");
+        ByteArrayInputStream bis = PdfComponent.generarPdf(reserva);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "inline; filename=citiesreport.pdf");
+
+        return ResponseEntity
+            .ok()
+            .headers(headers)
+            .contentType(MediaType.APPLICATION_PDF)
+            .body(new InputStreamResource(bis));
+        }
 
 }
